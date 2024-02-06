466740aa8f59d2359e42f6c8a7f7ff16c526f0d6
TRJoseph <steppingbeast@gmail.com>
2024-02-05 21:52:44 -0500

    swapped to using precomputed move tables. This commit contains both king and knight precomputed bitboard move tables

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -101,14 +101,14 @@ namespace Chess
 
             /* knight move offsets
              * Northeasteast, represents a knight move in an L shape that is one square up and two squares right */
-            public readonly ulong NorthNorthEast(ulong bitboard) { return (bitboard << 17) & AFileMask; }
-            public readonly ulong NorthEastEast(ulong bitboard) { return (bitboard << 10) & ABFileMask; }
-            public readonly ulong SouthEastEast(ulong bitboard) { return (bitboard >> 6) & ABFileMask; }
-            public readonly ulong SouthSouthEast(ulong bitboard) { return (bitboard >> 15) & AFileMask; }
-            public readonly ulong NorthNorthWest(ulong bitboard) { return (bitboard << 15) & HFileMask; }
-            public readonly ulong NorthWestWest(ulong bitboard) { return (bitboard << 6) & GHFileMask; }
-            public readonly ulong SouthWestWest(ulong bitboard) { return (bitboard >> 10) & GHFileMask; }
-            public readonly ulong SouthSouthWest(ulong bitboard) { return (bitboard >> 17) & HFileMask; }
+            public readonly ulong NorthNorthEast(ulong bitboard) { return (bitboard << 17) & HFileMask; }
+            public readonly ulong NorthEastEast(ulong bitboard) { return (bitboard << 10) & GHFileMask; }
+            public readonly ulong SouthEastEast(ulong bitboard) { return (bitboard >> 6) & GHFileMask; }
+            public readonly ulong SouthSouthEast(ulong bitboard) { return (bitboard >> 15) & HFileMask; }
+            public readonly ulong NorthNorthWest(ulong bitboard) { return (bitboard << 15) & AFileMask; }
+            public readonly ulong NorthWestWest(ulong bitboard) { return (bitboard << 6) & ABFileMask; }
+            public readonly ulong SouthWestWest(ulong bitboard) { return (bitboard >> 10) & ABFileMask; }
+            public readonly ulong SouthSouthWest(ulong bitboard) { return (bitboard >> 17) & ABFileMask; }
 
         }
 
@@ -180,6 +180,140 @@ namespace Chess
             ["F8"] = 1UL << 61,
             ["G8"] = 1UL << 62,
             ["H8"] = 1UL << 63
+        };
+
+        public static ulong[] PrecomputedKingMoves = {
+            0x302,
+            0x705,
+            0xe0a,
+            0x1c14,
+            0x3828,
+            0x7050,
+            0xe0a0,
+            0xc040,
+            0x30203,
+            0x70507,
+            0xe0a0e,
+            0x1c141c,
+            0x382838,
+            0x705070,
+            0xe0a0e0,
+            0xc040c0,
+            0x3020300,
+            0x7050700,
+            0xe0a0e00,
+            0x1c141c00,
+            0x38283800,
+            0x70507000,
+            0xe0a0e000,
+            0xc040c000,
+            0x302030000,
+            0x705070000,
+            0xe0a0e0000,
+            0x1c141c0000,
+            0x3828380000,
+            0x7050700000,
+            0xe0a0e00000,
+            0xc040c00000,
+            0x30203000000,
+            0x70507000000,
+            0xe0a0e000000,
+            0x1c141c000000,
+            0x382838000000,
+            0x705070000000,
+            0xe0a0e0000000,
+            0xc040c0000000,
+            0x3020300000000,
+            0x7050700000000,
+            0xe0a0e00000000,
+            0x1c141c00000000,
+            0x38283800000000,
+            0x70507000000000,
+            0xe0a0e000000000,
+            0xc040c000000000,
+            0x302030000000000,
+            0x705070000000000,
+            0xe0a0e0000000000,
+            0x1c141c0000000000,
+            0x3828380000000000,
+            0x7050700000000000,
+            0xe0a0e00000000000,
+            0xc040c00000000000,
+            0x203000000000000,
+            0x507000000000000,
+            0xa0e000000000000,
+            0x141c000000000000,
+            0x2838000000000000,
+            0x5070000000000000,
+            0xa0e0000000000000,
+            0x40c0000000000000
+        };
+
+        public static ulong[] PrecomputedKnightMoves = {
+            0x20400,
+            0x50800,
+            0xa1100,
+            0x142200,
+            0x284400,
+            0x508800,
+            0xa01000,
+            0x402000,
+            0x2040004,
+            0x5080008,
+            0xa110011,
+            0x14220022,
+            0x28440044,
+            0x50880088,
+            0xa0100010,
+            0x40200020,
+            0x204000402,
+            0x508000805,
+            0xa1100110a,
+            0x1422002214,
+            0x2844004428,
+            0x5088008850,
+            0xa0100010a0,
+            0x4020002000,
+            0x20400040200,
+            0x50800080500,
+            0xa1100110a00,
+            0x142200221400,
+            0x284400442800,
+            0x508800885000,
+            0xa0100010a000,
+            0x402000200000,
+            0x2040004020000,
+            0x5080008050000,
+            0xa1100110a0000,
+            0x14220022140000,
+            0x28440044280000,
+            0x50880088500000,
+            0xa0100010a00000,
+            0x40200020000000,
+            0x204000402000000,
+            0x508000805000000,
+            0xa1100110a000000,
+            0x1422002214000000,
+            0x2844004428000000,
+            0x5088008850000000,
+            0xa0100010a0000000,
+            0x4020002000000000,
+            0x400040200000000,
+            0x800080500000000,
+            0x1100110a00000000,
+            0x2200221400000000,
+            0x4400442800000000,
+            0x8800885000000000,
+            0x100010a000000000,
+            0x2000200000000000,
+            0x4020000000000,
+            0x8050000000000,
+            0x110a0000000000,
+            0x22140000000000,
+            0x44280000000000,
+            0x88500000000000,
+            0x10a00000000000,
+            0x20000000000000,
         };
 
         public static ulong testString = pieceLookupTable["A1"];
@@ -1001,15 +1135,18 @@ namespace Chess
 
             //ulong validKnightMoves = ComputeKnightMoves(InternalBoard.WhiteKnights);
             ulong whiteKnights = InternalBoard.WhiteKnights;
-            while(whiteKnights != 0)
+            while (whiteKnights != 0)
             {
-
 
                 whiteKnights &= whiteKnights - 1;
             }
-            
 
-            ulong validKingMoves = ComputeKingMoves(InternalBoard.WhiteKing);
+
+            //ulong validKingMoves = ComputeKingMoves(InternalBoard.WhiteKing);
+
+            ulong validKingMoves = PrecomputedKingMoves[(int)Math.Log(InternalBoard.WhiteKing, 2)];
+            validKingMoves = BoardManager.whiteToMove ? validKingMoves & ~InternalBoard.AllWhitePieces : validKingMoves & ~InternalBoard.AllBlackPieces;
+
             int startingSquare = (int)Math.Log(InternalBoard.WhiteKing, 2);
 
             while (validKingMoves != 0)
@@ -1065,7 +1202,7 @@ namespace Chess
             ulong square7 = InternalBoard.SouthWestWest(knight_loc);
             ulong square8 = InternalBoard.SouthEastEast(knight_loc);
 
-            
+
             ulong knightMoves = square1 | square2 | square3 | square4 | square5 | square6 | square7 | square8;
 
             ulong knightValidMoves = BoardManager.whiteToMove ? knightMoves & ~InternalBoard.AllWhitePieces : knightMoves & ~InternalBoard.AllBlackPieces;
