aabfe646f06da4fcc7635a71fb859214d75eba0b
TRJoseph <steppingbeast@gmail.com>
2024-04-24 23:57:00 -0400

    progress towards modifying the test suite to work with the fen match log

--- EngineMatchupApp/Assets/Scripts/GameManagement/GameManagement.cs
+++ EngineMatchupApp/Assets/Scripts/GameManagement/GameManagement.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 using static Chess.Board;
 using static Chess.MoveGen;
 using System.IO;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Chess
@@ -19,12 +20,37 @@ namespace Chess
             OnGameCompleted?.Invoke();
         }
 
+        // this FENStringList list will hold all 1000 chess matches' fen strings. this will be used in the test suite
+        static List<string> FENStringList = new List<string>();
+
+        public static void LoadFENStringList()
+        {
+            string path = @"../EngineMatchupApp/Assets/Scripts/GameManagement/lichessGameResults.txt";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // gets FENString from log
+                    if (line.Contains("FENString"))
+                    {
+                        // the FEN String text is separated from the actual FEN String by a colon
+                        int positionOfSeparator = line.IndexOf(':');
+                        FENStringList.Add(line.Substring(positionOfSeparator + 1));
+                    }
+                }
+            }
+        }
+
         public static void ComputerVsComputerMatches(int numOfMatches)
         {
+            // first load in the test game list from the test result log
+            LoadFENStringList();
+
             string logFilePath = Path.Combine(Application.dataPath, "../Logs/ChessMatchResults.txt");
             File.AppendAllText(logFilePath, $"Starting {numOfMatches} matches:\n");
 
-            int matchCount = 1;
+            int matchCount = 0;
             int totalWhiteWins = 0;
             int totalBlackWins = 0;
             int totalDraws = 0;
@@ -32,10 +58,10 @@ namespace Chess
             OnGameCompleted = () =>
             {
                 RecordResult(matchCount, logFilePath, ref totalWhiteWins, ref totalBlackWins, ref totalDraws);
-                if (matchCount < numOfMatches)
+                if (matchCount < numOfMatches - 1)
                 {
                     matchCount++;
-                    Arbiter.StartGame("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", true);
+                    Arbiter.StartGame(FENStringList[matchCount], true);
                 }
                 else
                 {
@@ -50,7 +76,7 @@ namespace Chess
                 }
             };
 
-            Arbiter.StartGame("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", true);
+            Arbiter.StartGame(FENStringList[matchCount], true);
         }
 
 
@@ -97,66 +123,66 @@ namespace Chess
     }
 }
 
-    //    public static void ComputerVsComputerMatches(int numOfMatches)
-    //    {
-    //        string logFilePath = Path.Combine(Application.dataPath, "../Logs/ChessMatchResults.txt");
+//    public static void ComputerVsComputerMatches(int numOfMatches)
+//    {
+//        string logFilePath = Path.Combine(Application.dataPath, "../Logs/ChessMatchResults.txt");
 
-    //        int totalWhiteWins = 0;
-    //        int totalBlackWins = 0;
-    //        int totalDraws = 0;
+//        int totalWhiteWins = 0;
+//        int totalBlackWins = 0;
+//        int totalDraws = 0;
 
-    //        using StreamWriter writer = new(logFilePath, true);
-    //        writer.WriteLine($"Starting {numOfMatches} matches:");
+//        using StreamWriter writer = new(logFilePath, true);
+//        writer.WriteLine($"Starting {numOfMatches} matches:");
 
-    //        for (int match = 1; match <= numOfMatches; match++)
-    //        {
-    //            Arbiter.StartGame("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", true);
+//        for (int match = 1; match <= numOfMatches; match++)
+//        {
+//            Arbiter.StartGame("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", true);
 
-    //            string resultLine = $"Match {match}: ";
-    //            switch (PositionInformation.currentStatus)
-    //            {
-    //                case GameResult.CheckMate:
-    //                    if (!PositionInformation.whiteToMove)
-    //                    {
-    //                        resultLine += "White Wins by Checkmate!";
-    //                        totalWhiteWins++;
-    //                    }
-    //                    else
-    //                    {
-    //                        resultLine += "Black Wins by Checkmate!";
-    //                        totalBlackWins++;
-    //                    }
-    //                    break;
-    //                case GameResult.Stalemate:
-    //                    resultLine += "Draw by Stalemate!";
-    //                    totalDraws++;
-    //                    break;
-    //                case GameResult.ThreeFold:
-    //                    resultLine += "Draw by Threefold Repetition!";
-    //                    totalDraws++;
-    //                    break;
-    //                case GameResult.FiftyMoveRule:
-    //                    resultLine += "Draw by Fifty Move Rule!";
-    //                    totalDraws++;
-    //                    break;
-    //                case GameResult.InsufficientMaterial:
-    //                    resultLine += "Draw by Insufficient Material!";
-    //                    totalDraws++;
-    //                    break;
-    //                default:
-    //                    resultLine += "Game still in progress or unknown result.";
-    //                    break;
-    //            }
+//            string resultLine = $"Match {match}: ";
+//            switch (PositionInformation.currentStatus)
+//            {
+//                case GameResult.CheckMate:
+//                    if (!PositionInformation.whiteToMove)
+//                    {
+//                        resultLine += "White Wins by Checkmate!";
+//                        totalWhiteWins++;
+//                    }
+//                    else
+//                    {
+//                        resultLine += "Black Wins by Checkmate!";
+//                        totalBlackWins++;
+//                    }
+//                    break;
+//                case GameResult.Stalemate:
+//                    resultLine += "Draw by Stalemate!";
+//                    totalDraws++;
+//                    break;
+//                case GameResult.ThreeFold:
+//                    resultLine += "Draw by Threefold Repetition!";
+//                    totalDraws++;
+//                    break;
+//                case GameResult.FiftyMoveRule:
+//                    resultLine += "Draw by Fifty Move Rule!";
+//                    totalDraws++;
+//                    break;
+//                case GameResult.InsufficientMaterial:
+//                    resultLine += "Draw by Insufficient Material!";
+//                    totalDraws++;
+//                    break;
+//                default:
+//                    resultLine += "Game still in progress or unknown result.";
+//                    break;
+//            }
 
-    //            writer.WriteLine(resultLine);
-    //            //UnityEngine.Debug.Log(resultLine); // Also log to Unity Console
-    //        }
+//            writer.WriteLine(resultLine);
+//            //UnityEngine.Debug.Log(resultLine); // Also log to Unity Console
+//        }
 
-    //        writer.WriteLine("Matches completed.");
-    //        writer.WriteLine("Total White Wins: " + totalWhiteWins);
-    //        writer.WriteLine("Total Black Wins: " + totalBlackWins);
-    //        writer.WriteLine("Total Draws: " + totalDraws);
-    //        writer.WriteLine("");
-    //    }
+//        writer.WriteLine("Matches completed.");
+//        writer.WriteLine("Total White Wins: " + totalWhiteWins);
+//        writer.WriteLine("Total Black Wins: " + totalBlackWins);
+//        writer.WriteLine("Total Draws: " + totalDraws);
+//        writer.WriteLine("");
+//    }
 
-    //}
+//}
